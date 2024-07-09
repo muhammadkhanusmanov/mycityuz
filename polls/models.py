196079d721ca391d11eeed5bf067193b14cda2ec
username <email>
2024-07-09 23:45:45 +0500

    post model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    branch_choices = [
+        ('Ijara','IJARA'),
+        ('Oldi-sotdi','OLDI-SOTDI'),
+        ('Ishchi','ISHCHI')
+    ]
+    
+    title = models.CharField(max_length=50)
+    branch = models.CharField(max_length=12,choices=branch_choices)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,related_name='posts')
+    description = models.CharField(max_length=350)
+    location = models.CharField(max_length=80)
+    pic1 = models.CharField(max_length=80)
+    pic2 = models.CharField(max_length=80, blank=True, null=True)
+    pic3 = models.CharField(max_length=80, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
