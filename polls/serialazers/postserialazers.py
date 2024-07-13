d56086b511d48e9eae7a1ef2eaca31189e0255f5
username <email>
2024-07-13 19:41:02 +0500

    serialzer for pos model

--- polls/serialazers/postserialazers.py
+++ polls/serialazers/postserialazers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from ..models import Post
-from .userserializers.serialazers import UserSerializer
+from .userserializers import UserSerializer
+
 
 class PostSerializer(serializers.ModelSerializer):
     pic1 = serializers.ImageField(required=True)
     pic2 = serializers.ImageField(required=False)
     pic3 = serializers.ImageField(required=False)
-    owner = UserSerializer()
-    
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['title', 'branch', 'owner', 'description', 'location', 'pic1', 'pic2', 'pic3']
 
+class PostListSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
+    class Meta:
+        model = Post
+        fields = ['title', 'branch', 'owner', 'description', 'location', 'pic1', 'pic2', 'pic3']
--- polls/serialazers/userserializers.py
+++ polls/serialazers/userserializers.py
@@ -1,12 +1,13 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField, ReadOnlyField
 from django.contrib.auth.models import User
+from rest_framework import serializers
 
-class AdminSerializer(ModelSerial):
+class AdminSerializer(ModelSerializer):
     role = serializers.CharField(source='last_name')
     full_name = serializers.CharField(source='first_name')
     class Meta:
         model = User
-        fields = ['id','username','full_name','role','email','created_at']
+        fields = ['id','username','full_name','role','email']
 
 class UserSerializer(ModelSerializer):
     full_name = serializers.CharField(source='first_name')
