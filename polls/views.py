0b2669a8154a38826996fb760853631b234d5fc9
username <email>
2024-07-13 19:42:20 +0500

    list of users

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from rest_framework.authtoken.models import Token
 from django.http import FileResponse
 from rest_framework.parsers import MultiPartParser, FormParser
 from django.contrib.auth.models import User
+from rest_framework import generics, filters
 from django.contrib.auth.hashers import make_password
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
@@ -14,7 +15,7 @@ from django.db.models import F
 from rest_framework.authentication import SessionAuthentication
 
 from .serialazers.userserializers import UserSerializer,AdminSerializer
-from .serialazers.postserialazers import PostSerializer
+from .serialazers.postserialazers import PostSerializer,PostListSerializer
 from .models import Post
 from .connect import upload_image_to_dropbox
 
@@ -30,29 +31,34 @@ class LoginView(APIView):
         return Response(rsp,status=status.HTTP_200_OK) 
 
 
-class MyModelCreateView(APIView):
+class PostCreateView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = PostSerializer(data=request.data)
         
         if serializer.is_valid():
             data = serializer.validated_data
 
-            pic1_url = upload_image_to_dropbox(data['pic1'].temporary_file_path(), 'pic1.jpg')
-            pic2_url = upload_image_to_dropbox(data['pic2'].temporary_file_path(), 'pic2.jpg') if data.get('pic2') else None
-            pic3_url = upload_image_to_dropbox(data['pic3'].temporary_file_path(), 'pic3.jpg') if data.get('pic3') else None
+            pic1_url = upload_image_to_dropbox(data['pic1'], data['pic1'].name)
+            pic2_url = upload_image_to_dropbox(data['pic2'], data['pic2'].name) if data.get('pic2') else None
+            pic3_url = upload_image_to_dropbox(data['pic3'], data['pic3'].name) if data.get('pic3') else None
 
             post = Post.objects.create(
                 title=data['title'],
                 branch=data['branch'],
+                owner=data['owner'],
                 description=data['description'],
                 location=data['location'],
                 pic1=pic1_url,
                 pic2=pic2_url,
                 pic3=pic3_url
             )
-            post.save()
-            
-            return Response(PostSerializer(post).data, status=status.HTTP_201_CREATED)
+
+            return Response(PostListSerializer(post).data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class ListUsers(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = AdminSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['username', 'email', 'first_name', 'last_name']
