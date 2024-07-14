9e6f5af6f1752bebe1418e738e06da95bfc084af
username <email>
2024-07-14 10:44:57 +0500

    the urls for delete user and post

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from polls.views import PostCreateView,LoginView,ListUsers
+from polls.views import PostCreateView,LoginView,ListUsers,DeleteUser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/',LoginView.as_view()),
     path('post/add/',PostCreateView.as_view()),
-    path('users/',ListUsers.as_view())
+    path('users/',ListUsers.as_view()),
+    path('delete/user/',DeleteUser.as_view()),
 ]
