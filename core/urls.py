89229a7873c6d33b68fd7899b036b14eb846bc13
username <email>
2024-07-27 19:51:09 +0500

    enpoint for get post

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from polls.views import (PostCreateView,LoginView,ListUsers,DeleteUser,DeletePost,
-    UserPosts,ListPosts,CountView,GoogleLogin,GoogleRegister,ChangeProfile)
+    UserProfile,ListPosts,CountView,GoogleLogin,GoogleRegister,ChangeProfile,
+    GetPost)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,10 +12,11 @@ urlpatterns = [
     path('users/',ListUsers.as_view()),
     path('delete/user/',DeleteUser.as_view()),
     path('delete/post/',DeletePost.as_view()),
-    path('user/posts/',UserPosts.as_view()),
+    path('user/posts/',UserProfile.as_view()),
     path('list/posts/',ListPosts.as_view()),
     path('view/post/',CountView.as_view()),
     path('google/login/',GoogleLogin.as_view()),
     path('google/register/',GoogleRegister.as_view()),
-    path('edit/profile/',ChangeProfile.as_view())
+    path('edit/profile/',ChangeProfile.as_view()),
+    path('get/post/', GetPost.as_view())
 ]
