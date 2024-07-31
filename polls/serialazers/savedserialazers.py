97b439b271d28cbe8b37fb95ce42e8bf7875807a
username <email>
2024-07-31 20:09:52 +0500

    changes at simpleserialzers

--- polls/serialazers/savedserialazers.py
+++ polls/serialazers/savedserialazers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from ..models import Saved
 from .userserializers import UserSerializer
-from ..postserialazers import PostSimpleSerializer
+from .postserialazers import PostSimpleSerializer
 
 class SavedSerializer(serializers.ModelSerializer):
     posts = PostSimpleSerializer(many=True)
     class Meta:
         model = Saved
         fields = ['id','posts']
+
         
         
